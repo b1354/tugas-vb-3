e79df2d51bbf5ad3729cc5f8ff92869cc1ee4b26
b1354 <bayurizkyseptiansyah@gmail.com>
2024-12-10 10:21:57 +0700

    melakukan refactoring ulang

--- src/main/java/org/test/tugas/registrasi/Registrasi.java
+++ src/main/java/org/test/tugas/registrasi/Registrasi.java
@@ -11,38 +11,32 @@ import org.test.tugas.registrasi.config.Koneksi;
 public class Registrasi {
     Koneksi koneksi = new Koneksi();
     JFrame jf = new JFrame("Registrasi");
+    JLabel jl_judul = new JLabel("Registrasi");
+    TextInput tf_nim = new TextInput("nim");
+    TextInput tf_nama = new TextInput("nama");
+    TextInput tf_jurusan = new TextInput("jurusan");
+    JButton btn_cari = new JButton("Cari");
+    JButton btn_tambah = new JButton("Tambah");
+    JButton btn_ubah = new JButton("ubah");
+    JButton btn_hapus = new JButton("hapus");
     
     public Registrasi() {
-        jf.setLayout(null);
-        jf.setResizable(false);;
-
         if (!koneksi.connected) {
             String errorMessage = "Terjadi Error,\n Tidak dapat terhubung ke database";
             JOptionPane.showMessageDialog(jf, errorMessage, "Error", 0);
             return;
         }
-        
-        JLabel jl_judul = new JLabel("Registrasi");
-        TextInput tf_nim = new TextInput("nim");
-        TextInput tf_nama = new TextInput("nama");
-        TextInput tf_jurusan = new TextInput("jurusan");
-
-        JButton btn_cari = new JButton("Cari");
-        JButton btn_tambah = new JButton("Tambah");
-        JButton btn_ubah = new JButton("ubah");
-        JButton btn_hapus = new JButton("hapus");
 
         tf_nim.setWidth(100);
 
         jl_judul.setBounds(125, 10, 150, 50);
-        btn_cari.setBounds(180, 70, 90, 20);
-        btn_tambah.setBounds(20, 160, 70, 20);
-        btn_ubah.setBounds(110, 160, 70, 20);
-        btn_hapus.setBounds(200, 160, 70, 20);
-
         tf_nama.add(jf, 20, 100);
+        btn_cari.setBounds(180, 70, 90, 20);
         tf_jurusan.add(jf, 20, 130);
         tf_nim.add(jf, 20, 70);
+        btn_tambah.setBounds(20, 160, 70, 20);
+        btn_ubah.setBounds(110, 160, 70, 20);
+        btn_hapus.setBounds(200, 160, 70, 20);        
         
         jf.add(jl_judul);
         jf.add(btn_cari);
@@ -50,6 +44,8 @@ public class Registrasi {
         jf.add(btn_ubah);
         jf.add(btn_hapus);
 
+        jf.setLayout(null);
+        jf.setResizable(false);
         jf.setSize(300, 300);
         jf.setVisible(true);
     }
