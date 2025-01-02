e42d6c2e2daabc6fbdd317962b88e077136d2e8e
b1354 <bayurizkyseptiansyah@gmail.com>
2025-01-02 11:18:05 +0700

    mengubah kode utama sesuai dengan kode yang digunakan di kelas

--- src/main/java/org/test/tugas/registrasi/MainApp.java
+++ src/main/java/org/test/tugas/registrasi/MainApp.java
@@ -1,9 +1,7 @@
 package org.test.tugas.registrasi;
 
-import org.test.tugas.registrasi.view.Registrasi;
-
 public class MainApp {
     public static void main(String[] args) {
-        Registrasi reg = new Registrasi();
+        Registrasi regisrasi = new Registrasi();   
     }
 }
